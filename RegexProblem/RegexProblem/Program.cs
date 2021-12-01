bcff5f43ef606d5f97c055804741beb971110392
suraj9982 <surajsk9982@gmail.com>
2021-12-01 22:39:02 +0530

    [suraj] add : It validates the LastName

--- RegexProblem/RegexProblem/Program.cs
+++ RegexProblem/RegexProblem/Program.cs
@@ -10,20 +10,25 @@ namespace RegexProblem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE OPTION\n1.Validating Firstname\n2.Exit");
+                Console.WriteLine("CHOOSE OPTION\n1.Validating Firstname\n2.Validate LastName\n3.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("Enter the firstname");
+                        Console.WriteLine("Enter the Firstname");
                         string firstname = Console.ReadLine();
                         regex.ValidateName(firstname);
                         break;
                     case 2:
+                        Console.WriteLine("Enter the Lastname");
+                        string lastname= Console.ReadLine();
+                        regex.ValidateName(lastname);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
-                        Console.WriteLine("your choice should be between 1 to 2 ");
+                        Console.WriteLine("your choice should be between 1 to 3 ");
                         break;
                 }
 
