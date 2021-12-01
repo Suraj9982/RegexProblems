fc6ff0a8df18a711132f54bca707419301b1bdb2
suraj9982 <surajsk9982@gmail.com>
2021-12-01 22:31:47 +0530

    [suraj] add ;  It validate the firstname

---
+++ RegexProblem/RegexProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RegexProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleRegex regex = new SampleRegex();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE OPTION\n1.Validating Firstname\n2.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the firstname");
+                        string firstname = Console.ReadLine();
+                        regex.ValidateName(firstname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 2 ");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ RegexProblem/RegexProblem/SampleRegex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    class SampleRegex
+    {
+        string NamePattern = "^[A-Z][a-z]{2,}$";
+        public void ValidateName(string Firstname)
+        {
+            Regex regex = new Regex(NamePattern);
+            bool result = regex.IsMatch(Firstname);
+            if (result)
+            {
+                Console.WriteLine("firstname is valid");
+            }
+            else
+            {
+                Console.WriteLine("please enter valid firstname or firstname must have starts with capital letter");
+            }
+        }
+    }
+}
